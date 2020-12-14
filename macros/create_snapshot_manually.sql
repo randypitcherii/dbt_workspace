d2dbaaa6b0a35d6138145cc2174fcd5ef50511b1
Randy Pitcher <randy.pitcher@fishtownanalytics.com>
2020-12-14 23:30:16 +0000

    updated snapshot macro

--- macros/create_snapshot_manually.sql
+++ macros/create_snapshot_manually.sql
@@ -2,7 +2,7 @@
 
 {% macro create_snapshot_manually(relation_to_snapshot) %}
 
-    {% set snapshot_target = target.database ~ '.' ~ target.schema ~ '.' ~ relation_to_snapshot.split('.')[-1] ~ '_SNAPSHOT' %}
+    {% set snapshot_target = target.database ~ '.' ~ target.schema ~ '.' ~ relation_to_snapshot.split('.')[-1] ~ '__SNAPSHOT' %}
 
     {% set create_snapshot_query %}
         CREATE TABLE IF NOT EXISTS {{snapshot_target}} AS (
@@ -16,7 +16,7 @@
 
 
     {% set insert_snapshot_query %}
-        INSERT INTO {{snapshot_target}} AS (
+        INSERT INTO {{snapshot_target}} (
             SELECT 
                 *,
                 CURRENT_TIMESTAMP AS SNAPSHOTTED_AT_TIME
@@ -29,5 +29,7 @@
 
     {% do log('Inserting full snapshot into target table from relation to snapshot --> ' ~ relation_to_snapshot, True) %}
     {% do run_query(insert_snapshot_query) %}
+
+    {% do run_query('COMMIT') %}
   
 {% endmacro %}
