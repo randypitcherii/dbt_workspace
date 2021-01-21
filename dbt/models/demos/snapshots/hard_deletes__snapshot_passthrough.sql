461a7eb4a7c07083a5ba0b8573f0557b74f914fe
Randy <randy.pitcher@fishtownanalytics.com>
2021-01-21 20:22:02 +0000

    added logic for distant_future/past ification of snapshots

--- dbt/models/demos/snapshots/hard_deletes__snapshot_passthrough.sql
+++ dbt/models/demos/snapshots/hard_deletes__snapshot_passthrough.sql
@@ -1,12 +1,22 @@
-/* select 
-    *
-    
-from {{ ref('hard_deletes__snapshot')}}
- */
-
+{% set distant_past='TIMESTAMP("1960-01-01 00:00:00+00")'%}
+{% set distant_future='TIMESTAMP("2100-01-01 00:00:00+00")'%}
+{% set min_valid_from='(select min(dbt_valid_from) from ' ~ ref('hard_deletes__snapshot') ~ ')'%}
 
  select 
-        id,
-        first_value(dbt_valid_to) over (partition by id order by dbt_valid_to asc ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as cnt
+    *,
+
+    if(
+        dbt_valid_from = {{ min_valid_from }},
+        {{distant_past}},
+        dbt_valid_from
+    ) as valid_from_with_distant_past,
+
+    if(
+        dbt_valid_to is null,
+        {{distant_future}},
+        dbt_valid_to
+    ) as valid_to_with_distant_future
+
+
     
-    from {{ ref('hard_deletes__snapshot')}}
+from {{ ref('hard_deletes__snapshot')}}
--- dbt/models/demos/snapshots/hard_deletes_source.sql
+++ dbt/models/demos/snapshots/hard_deletes_source.sql
@@ -2,13 +2,13 @@
 
 select
     1 as id,
-    'purple' as color
+    'blue' as color
 
-/* union all
+union all
 
 select
     2 as id,
-    'yellow' as color */
+    'yellow' as color
 
 union all
 
