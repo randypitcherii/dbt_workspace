1f02c73c7dd838778c08f9bc263bbf6a539b4759
Randy <randy.pitcher@fishtownanalytics.com>
2021-01-21 20:24:05 +0000

    readded the snapshot passthrough

--- dbt/models/demos/snapshots/hard_deletes__snapshot_passthrough.sql
+++ dbt/models/demos/snapshots/hard_deletes__snapshot_passthrough.sql
@@ -1,22 +1,4 @@
-{% set distant_past='TIMESTAMP("1960-01-01 00:00:00+00")'%}
-{% set distant_future='TIMESTAMP("2100-01-01 00:00:00+00")'%}
-{% set min_valid_from='(select min(dbt_valid_from) from ' ~ ref('hard_deletes__snapshot') ~ ')'%}
-
- select 
-    *,
-
-    if(
-        dbt_valid_from = {{ min_valid_from }},
-        {{distant_past}},
-        dbt_valid_from
-    ) as valid_from_with_distant_past,
-
-    if(
-        dbt_valid_to is null,
-        {{distant_future}},
-        dbt_valid_to
-    ) as valid_to_with_distant_future
-
-
+select 
+    *
     
 from {{ ref('hard_deletes__snapshot')}}
