0bb01c3c259f0c7ababf5f67c98f21299330905f
Randy <randy.pitcher@fishtownanalytics.com>
2021-01-27 15:30:02 +0000

    FIXED RESIZE WAREHOUSE

--- dbt/macros/utils/resize_warehouse.sql
+++ dbt/macros/utils/resize_warehouse.sql
@@ -1,9 +1,9 @@
 {#    
     Run the following dbt command to delete snapshots in this project:
-        dbt run-operation resize_warehouse('my_warehouse','xsmall')
+        dbt run-operation resize_warehouse --args '{warehouse_name: my_warehouse, warehouse_size: xsmall}'
 #}
 {% macro resize_warehouse(warehouse_name, warehouse_size) %}
-    {% set query = 'ALTER WAREHOUSE ' ~ warehouse_name ~ ' SET SIZE=' ~ warehouse_size ~ ';' %}
+    {% set query = 'ALTER WAREHOUSE ' ~ warehouse_name ~ ' SET WAREHOUSE_SIZE=' ~ warehouse_size ~ ';' %}
     {% if execute %}            
         {% do run_query(query) %}        
     {% endif %}
