c81ff831f51cde1ff77f5da2d2a18cafc004377c
Randy Pitcher <randy.pitcher@fishtownanalytics.com>
2021-05-15 19:11:18 +0000

    added stage loading with netflix data

---
+++ dbt/models/demos/netflix_data_loading/raw_netflix_data.sql
@@ -0,0 +1,20 @@
+{{ config(materialized='table') }}
+
+{% set target_stage='randy_pitcher_workspace_raw.netflix_loading.netflix_blob_stage'%}
+
+with 
+
+raw_data as (
+    select
+        {{ get_stage_columns_from_loading_config(
+            stage_name=target_stage, 
+            config_table=ref('loading_config')
+        )}},
+        current_timestamp::timestamp_ntz as ingestion_time
+    
+    from
+        @{{target_stage}}
+)
+
+
+select * from raw_data
