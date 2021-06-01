ab5d39206423ca96ba7fe278d08bfd33bfc2d1f6
Randy Pitcher <randypitcherii@gmail.com>
2021-06-01 18:15:53 -0400

    updated env vars for python dbt job runner

--- python/run_and_monitor_dbt_cloud_job.py
+++ python/run_and_monitor_dbt_cloud_job.py
@@ -5,14 +5,14 @@ import time
 #------------------------------------------------------------------------------
 # get environment variables
 #------------------------------------------------------------------------------
-api_base        = os.getenv('DBT_CLOUD_URL', 'https://cloud.getdbt.com') # default to multitenant url
-job_cause       = os.getenv('DBT_CLOUD_JOB_CAUSE', 'API-triggered job') # default to generic message
-git_branch      = os.getenv('DBT_CLOUD_JOB_BRANCH', None) # default to None
-schema_override = os.getenv('DBT_CLOUD_JOB_SCHEMA_OVERRIDE', None) # default to None
-api_key         = os.environ['DBT_CLOUD_API_KEY']  # no default here, just throw an error here if key not provided
-account_id      = os.environ['DBT_CLOUD_ACCOUNT_ID'] # no default here, just throw an error here if id not provided
-project_id      = os.environ['DBT_CLOUD_PROJECT_ID'] # no default here, just throw an error here if id not provided
-job_id          = os.environ['DBT_CLOUD_PR_JOB_ID'] # no default here, just throw an error here if id not provided
+api_base        = os.getenv('DBT_URL', 'https://cloud.getdbt.com') # default to multitenant url
+job_cause       = os.getenv('DBT_JOB_CAUSE', 'API-triggered job') # default to generic message
+git_branch      = os.getenv('DBT_JOB_BRANCH', None) # default to None
+schema_override = os.getenv('DBT_JOB_SCHEMA_OVERRIDE', None) # default to None
+api_key         = os.environ['DBT_API_KEY']  # no default here, just throw an error here if key not provided
+account_id      = os.environ['DBT_ACCOUNT_ID'] # no default here, just throw an error here if id not provided
+project_id      = os.environ['DBT_PROJECT_ID'] # no default here, just throw an error here if id not provided
+job_id          = os.environ['DBT_PR_JOB_ID'] # no default here, just throw an error here if id not provided
 #------------------------------------------------------------------------------
 
 
