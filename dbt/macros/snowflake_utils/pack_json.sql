7449fdbb2c549fd2893d35231cec3cae6b2b4de1
Brandon Thomson <bthomson22@gmail.com>
2021-07-01 14:18:19 -0600

    Added exclude feature to pack_json (#37)
    Co-authored-by: Brandon Thomson <brandon@brandon-thomson.lan>

--- dbt/macros/snowflake_utils/pack_json.sql
+++ dbt/macros/snowflake_utils/pack_json.sql
@@ -1,3 +1,20 @@
-{% macro pack_json() %}
-    OBJECT_CONSTRUCT(*)::VARIANT
-{% endmacro %}
+{% macro pack_json(from, relation_alias=False, except=[]) -%}
+    {%- set include_cols = [] %}
+    {%- set cols = adapter.get_columns_in_relation(from) -%}
+
+    {%- for col in cols -%}
+
+        {%- if col.column not in except -%}
+            {% do include_cols.append(col.column) %}
+
+        {%- endif %}
+    {%- endfor %}
+
+    object_construct_keep_null(
+    {%- for col in include_cols %}        
+        
+        {%- if relation_alias %}{{ relation_alias }}.{% else %}{%- endif -%}{{"'" + col + "'" + ', ' + col }}
+        {%- if not loop.last %},{{'\n  ' }}{% endif %}
+    {%- endfor -%}
+    )
+{%- endmacro %}
