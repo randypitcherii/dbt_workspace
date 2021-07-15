6d3fb9641a899ba039511a3782cab2339c56ac25
Randy Pitcher <randy.pitcher@fishtownanalytics.com>
2021-07-15 15:45:26 +0000

    fixed the build!

--- dbt/models/demos/pack_json/pack_json_demo.sql
+++ dbt/models/demos/pack_json/pack_json_demo.sql
@@ -1,6 +1,8 @@
 select 
     warehouse_name, 
     warehouse_id,
-    {{pack_json(ref('stg_warehouse_metering_history'))}} as json
+    {{pack_json(ref('stg_warehouse_metering_history'), except=['json'])}} as json
     
 from {{ref('stg_warehouse_metering_history')}}
+
+-- {{this}}
