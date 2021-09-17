0b1ed152eee41f84d9bc218b839e22080359b52c
Randy <randypitcherii@gmail.com>
2021-09-17 13:55:30 -0500

    updated check cols

--- dbt/snapshots/hard_deletes/hard_deletes__snapshot.sql
+++ dbt/snapshots/hard_deletes/hard_deletes__snapshot.sql
@@ -5,7 +5,7 @@
       target_database=target.database,
       target_schema=target.schema,
       unique_key='order_id',
-      check_cols=['color'],
+      check_cols=['color', 'status', 'ORDER_DATE'],
       strategy='check',
       invalidate_hard_deletes=True,
     )
