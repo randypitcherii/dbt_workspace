5470a7c07fcf94708dd44173dd93905ffe28a9a1
Randy Pitcher <randypitcherii@gmail.com>
2022-04-26 10:37:25 -0400

    serverless init

---
+++ serverless/handler.py
@@ -0,0 +1,12 @@
+import json
+
+
+def get_api_results(event, context):
+    body = {
+        "message": "Go Serverless v3.0! Your function executed successfully!",
+        "input": event,
+    }
+
+    response = {"statusCode": 200, "body": json.dumps(body)}
+
+    return response
