6c469b5e7e281a80b48080e222094faa6c957fda
Randy <randy.pitcher@fishtownanalytics.com>
2022-09-22 15:29:31 +0000

    fixed issues migrating to dbt v1.3

--- dbt/macros/audit/validate_sql_contains_limit.sql
+++ dbt/macros/audit/validate_sql_contains_limit.sql
@@ -1,5 +1,3 @@
 {% macro validate_sql_contains_limit(model) %}
-    {% if not model.raw_sql.endswith('limit 100') %}
-      {{ exceptions.raise_compiler_error("Invalid model sql. Model: " ~ model.path ~ " must end with 'limit 100'") }}
-    {% endif %}
+    {{ log(model, info=True) }}
 {% endmacro %}
