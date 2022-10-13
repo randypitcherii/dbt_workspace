f9c4517063a21e5893bcfc58e176c45c32c68815
Randy <randy.pitcher@fishtownanalytics.com>
2022-10-13 15:04:39 +0000

    python progress

--- dbt/models/demos/python/manifest_parsed.py
+++ dbt/models/demos/python/manifest_parsed.py
@@ -1,5 +1,11 @@
+import pandas as pd
+
 def model(dbt, session):
     dbt.config(materialized="table")
 
-    df = dbt.ref("my_first_model")
-    return df
+    df_zodiac = dbt.ref('zodiac').to_pandas()
+
+    # explode the rows with daily resolution
+    df_zodiac['DATE_DAY'] = pd.to_datetime(df_zodiac['DATE_DAY'])
+
+    return df_zodiac
