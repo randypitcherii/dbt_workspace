a0fe3ffa7f30679fc39d6e8a8d1c00c2e6eb41a8
Randy <randypitcherii@gmail.com>
2022-10-18 14:49:10 -0400

    locked! (#69)
    Co-authored-by: Randy <randy.pitcher@fishtownanalytics.com>

--- dbt/models/coalesce-2022/3--miids/b__miid_unprocessed_rows.sql
+++ dbt/models/coalesce-2022/3--miids/b__miid_unprocessed_rows.sql
@@ -3,7 +3,7 @@ select
     zodiac_sign || ' - ' || color as natural_key,
     *
 
-from {{ ref('incremental_source') }}
+from {{ ref('a__incremental_source') }}
 
 where 
     processed_at > (select coalesce(max(a_miid.processed_at),0::timestamp) from {{ ref('a__miid') }} a_miid) 
--- dbt/models/coalesce-2022/coalesce_tidy_up.sql
+++ dbt/models/coalesce-2022/coalesce_tidy_up.sql
@@ -1,3 +1,4 @@
+{{config(enabled=False)}}
 
 select 'all clean!' as msg 
 
